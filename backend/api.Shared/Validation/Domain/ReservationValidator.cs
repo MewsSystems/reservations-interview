407272095e3b66b4ef7f23c196f900c68081d36f
Toni Tosic <ttosic92@gmail.com>
2024-09-03 10:50:36 +0200

    fix(backend): Fix room number validator.

--- backend/api.Shared/Validation/Domain/ReservationValidator.cs
+++ backend/api.Shared/Validation/Domain/ReservationValidator.cs
@@ -9,9 +9,9 @@ namespace api.Shared.Validation.Domain
         public ReservationValidator()
         {
             RuleFor(x => x.GuestEmail)
-                .NotNull()
-                .NotEmpty()
-                .EmailAddress()
+                .NotNull().WithMessage("Guest email cannot be null.")
+                .NotEmpty().WithMessage("Guest email cannot be empty.")
+                .EmailAddress().WithMessage("Guest email has to eb valid email address.")
                 .WithMessage("Invalid guest email address.");
             RuleFor(x => x.Id).Must((id) => id != Guid.Empty).WithMessage("Invalid reservation unique identifier.");
             RuleFor(r => r)
@@ -25,11 +25,18 @@ namespace api.Shared.Validation.Domain
                 .WithMessage("Reservation must be between 1 day and 30 days.");
 
             RuleFor(x => x.RoomNumber)
-                .NotNull()
-                .NotEmpty()
-                .Length(3)
-                .Matches(@"^[1-9][0-9][1-9]$")
-                .WithMessage("Room number must be a three-digit number with a non-zero floor number and non-'00' door number.");
+                .NotNull().WithMessage("Room number cannot be null.")
+                .NotEmpty().WithMessage("Room number cannot be empty.")
+                .Length(3).WithMessage("Room number must be exactly 3 digits.")
+                .Matches(@"^[0-9]{3}$").WithMessage("Room number must be exactly 3 digits.")
+                .Must(ValidateRoomNumber).WithMessage("Room number must be a valid three-digit number with a non-zero floor number and non-'00' door number.");
+        }
+
+        private bool ValidateRoomNumber(string roomNumber)
+        {
+            var floorNumber = int.Parse(roomNumber[0].ToString());
+            var doorNumber = int.Parse(roomNumber[1..]);
+            return floorNumber >= 0 && floorNumber <= 9 && doorNumber != 0;
         }
     }
 }
